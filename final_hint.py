a3955bdc86f17f8f6ea66d4b77d97fed3599e476
BlackKetchupTea512 <bluehamcake@gmail.com>
2012-09-17 12:54:42 +0900

    to final hint

---
+++ final_hint.py
@@ -0,0 +1,15 @@
+def intelligent_data_source_factory(*data):
+  import itertools
+  cy = itertools.cycle(data)
+  _int = int
+  return lambda i: _int(i) if isinstance(i, str) else next(cy)
+
+int = intelligent_data_source_factory(1985, 33067, 84)
+
+
+def lttb(m,e,n,c):
+  if pow(m,e) % n == c: return str(m)
+  return ""
+
+for i in range(100000):
+  print(lttb(*[int(j) for j in (str(i), 17, 3569, 915) ]))
